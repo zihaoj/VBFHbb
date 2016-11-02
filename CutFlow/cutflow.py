50bc6c32c4f1b401aacddef47dfe9dae52be8b82
zihaoj <zihaoj1991@gmail.com>
2016-11-02 20:10:24 +0100

    update

---
+++ CutFlow/cutflow.py
@@ -0,0 +1,17 @@
+from ROOT import *
+
+file = TFile("cutflow_ggF_2cen.root", "r")
+
+presel= file.Get("CutFlow/PreselectionCutFlow")
+
+for ibin in range(presel.GetNbinsX()):
+    print presel.GetBinContent(ibin+1)
+
+sel= file.Get("CutFlow/Nominal/CutsNoWeight")
+
+for ibin in range(presel.GetNbinsX()):
+    print sel.GetBinContent(ibin+1)
+
+
+
+    
